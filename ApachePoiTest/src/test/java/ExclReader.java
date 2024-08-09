0330f29e9ffcac481a26aa2aacbe1b24289b989b
Salman <hafiz.saleem@afs.com>
2024-08-09 18:13:04 -0400

    updates to excel date

--- ApachePoiTest/src/test/java/ExclReader.java
+++ ApachePoiTest/src/test/java/ExclReader.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
@@ -14,11 +17,35 @@ public class ExclReader {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		try {
+			
+			projectpath = System.getProperty("user.dir");
+			System.out.println(projectpath);// just to make sure we are getting right path of the project here.
+			workbook = new XSSFWorkbook(projectpath+File.separator+"Excel"+File.separator+"data.xlsx");
+			sheet = workbook.getSheet("sheet1");
+			
+			int rowCount = sheet.getPhysicalNumberOfRows(); //getting total number of rows
+			
+			for (int rowNumb = 1; rowNumb < rowCount; rowNumb++) {
+				
+				String usrm = sheet.getRow(rowNumb).getCell(0).getStringCellValue();
+				String pswd = sheet.getRow(rowNumb).getCell(1).getStringCellValue();
+				String role = sheet.getRow(rowNumb).getCell(2).getStringCellValue();
+				double exp  = sheet.getRow(rowNumb).getCell(3).getNumericCellValue();
+				System.out.println(usrm + " "+ pswd + " " + role + " "+ exp);
+				
+			}
+			
+			
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		projectpath = System.getProperty("user.dir");
-		System.out.println(projectpath);// just to make sure we are getting right path of the project here
 		
-
 	}
 
 }
