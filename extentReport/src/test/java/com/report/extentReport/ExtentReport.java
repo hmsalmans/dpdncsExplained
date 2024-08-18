88cb5aa97d620048e03008144d5b9acf8e943eb9
Salman <hafiz.saleem@afs.com>
2024-08-17 22:57:48 -0400

    adding extent reporting config

---
+++ extentReport/src/test/java/com/report/extentReport/ExtentReport.java
@@ -0,0 +1,36 @@
+package com.report.extentReport;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReport {
+
+	
+
+   
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+   
+        ExtentReports extent = new ExtentReports();
+       
+
+
+		
+		
+		
+		
+		
+		
+	}	
+		
+		
+	}
+
+
